ea813770089f836af255ace1f77b04075c18da09
mr-Ayalk <ayalkteketel7@gmail.com>
2026-01-08 01:27:05 +0300

    testimonial section done

--- landingpage/src/app/page.tsx
+++ landingpage/src/app/page.tsx
@@ -6,6 +6,7 @@ import HotDeliciousSection from "@/sections/HotDeliciousSection";
 import HowWeServe from "@/sections/HowWeServe";
 import Navbar from "@/sections/Navbar";
 import PromoCards from "@/sections/PromoCards";
+import Testimonial from "@/sections/Testimonial";
 import TrendingMenu from "@/sections/TrendingMenu";
 
 export default function Home() {
@@ -19,6 +20,7 @@ export default function Home() {
             <TrendingMenu />
             <AboutFood />
             <HowWeServe />
+            <Testimonial />
             <Footer />
         </div>
     );
